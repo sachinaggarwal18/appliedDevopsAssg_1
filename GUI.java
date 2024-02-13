665d915c30102ea8f2e48dbf461c92c3a8aaf6e5
hunterhhh412 <amanagnihotri412002@gmail.com>
2024-02-13 12:11:34 +0530

    Initial Commit.

---
+++ GUI.java
@@ -0,0 +1,278 @@
+package boldflame_assured.calculator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends JFrame {
+
+    JButton btnAdd, btnSubtract, btnDivide, btnMultiply, btnClear, btnDelete, btnEquals, btnDot;
+    JButton numBtn[];
+    JTextField output;
+    String previous, current, operator;
+    
+    public void processOutputNumber() {
+        if (current.length() > 0) {
+            String integerPart = current.split("\\.")[0];
+            String decimalPart = current.split("\\.")[1];
+            if (decimalPart.equals("0")) {
+                current = integerPart;
+            }
+        }
+    }
+
+    public void delete() {
+        if (current.length() > 0) {
+            current = current.substring(0, current.length() - 1);
+        }
+    }
+
+    public void clear() {
+        current = "";
+        previous = "";
+        operator = null;
+    }
+
+    public void updateOutput() {
+        output.setText(current);
+    }
+
+    public void appendToOutput(String num) {
+        if (num.equals(".") && current.contains(".")) {
+            return;
+        }
+        current += num;
+    }
+
+    public void selectOperator(String newOperator) {
+        // if no number is entered yet
+        if (current.isEmpty()) {
+            operator = newOperator;
+            return;
+        }
+
+        if (!previous.isEmpty()) {
+            calculate();
+        }
+
+        operator = newOperator;
+        previous = current;
+        current = "";
+    }
+
+    public void calculate() {
+        if (previous.length() < 1 || current.length() < 1) {
+            return;
+        }
+        double result = 0.0;
+        double num1 = Double.parseDouble(previous);
+        double num2 = Double.parseDouble(current);
+        switch (operator) {
+            case "*":
+                result = num1 * num2;
+                break;
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "÷":
+                if (num2 == 0) {
+                // Handle division by zero error
+                JOptionPane.showMessageDialog(this, "Error: Division by zero", "Error", JOptionPane.ERROR_MESSAGE);
+                clear();
+                updateOutput();
+                return;
+            }
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+        current = String.valueOf(result);
+        operator = null;
+        previous = "";
+        processOutputNumber();
+    }
+
+    private class NumberBtnHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton selectedBtn = (JButton) e.getSource();
+            for (JButton btn : numBtn) {
+                if (selectedBtn == btn) {
+                    appendToOutput(btn.getText());
+                    updateOutput();
+                }
+            }
+        }
+    }
+
+    private class OperatorBtnHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton selectedBtn = (JButton) e.getSource();
+            if (selectedBtn == btnMultiply) {
+                selectOperator(btnMultiply.getText());
+            } else if (selectedBtn == btnAdd) {
+                selectOperator(btnAdd.getText());
+            } else if (selectedBtn == btnSubtract) {
+                selectOperator(btnSubtract.getText());
+            } else if (selectedBtn == btnDivide) {
+                selectOperator(btnDivide.getText());
+            }
+            updateOutput();
+        }
+    }
+
+    private class OtherBtnHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton selectedBtn = (JButton) e.getSource();
+            if (selectedBtn == btnDelete) {
+                delete();
+            } else if (selectedBtn == btnClear) {
+                clear();
+            } else if (selectedBtn == btnEquals) {
+                calculate();
+            }
+            updateOutput();
+        }
+    }
+
+    public GUI() {
+        super("Simple GUI Calculator");
+        JPanel mainPanel = new JPanel();
+
+        // Initializing the calculator operands
+        current = "";
+        previous = "";
+
+        // Create sub panels inside main panel
+        JPanel row1 = new JPanel();
+        JPanel row2 = new JPanel();
+        JPanel row3 = new JPanel();
+        JPanel row4 = new JPanel();
+        JPanel row5 = new JPanel();
+
+        // Initialize components
+        output = new JTextField(16);
+        btnSubtract = new JButton("-");
+        btnAdd = new JButton("+");
+        btnDivide = new JButton("÷");
+        btnMultiply = new JButton("*");
+        btnDot = new JButton(".");
+        btnEquals = new JButton("=");
+        btnClear = new JButton("C");
+        btnDelete = new JButton("D");
+
+        // Instantiate action listeners
+        NumberBtnHandler numBtnHandler = new NumberBtnHandler();
+        OtherBtnHandler otherBtnHandler = new OtherBtnHandler();
+        OperatorBtnHandler opBtnHandler = new OperatorBtnHandler();
+
+        // Initialize, style, and add action listeners to number buttons
+        numBtn = new JButton[11];
+        numBtn[10] = btnDot;
+        for (int count = 0; count < numBtn.length - 1; count++) {
+            numBtn[count] = new JButton(String.valueOf(count));
+            numBtn[count].setFont(new Font("Monospaced", Font.BOLD, 22));
+            numBtn[count].addActionListener(numBtnHandler);
+        }
+
+        // Style other buttons
+        btnDot.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnEquals.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnAdd.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnSubtract.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnDivide.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnMultiply.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnClear.setFont(new Font("Monospaced", Font.BOLD, 20));
+        btnDelete.setFont(new Font("Monospaced", Font.BOLD, 20));
+
+        // Style the output display
+        output.setMaximumSize(new Dimension(185, 40));
+        output.setFont(new Font("Monospaced", Font.BOLD, 27));
+        output.setDisabledTextColor(new Color(0, 0, 0));
+        output.setMargin(new Insets(0, 5, 0, 0));
+        output.setText("0");
+
+        // Add action listeners to other buttons
+        btnDot.addActionListener(numBtnHandler);
+        btnDelete.addActionListener(otherBtnHandler);
+        btnClear.addActionListener(otherBtnHandler);
+        btnEquals.addActionListener(otherBtnHandler);
+
+        // Add action listeners to operation buttons
+        btnMultiply.addActionListener(opBtnHandler);
+        btnAdd.addActionListener(opBtnHandler);
+        btnSubtract.addActionListener(opBtnHandler);
+        btnDivide.addActionListener(opBtnHandler);
+
+        // Set the layout of each row in the pane
+        row1.setLayout(new BoxLayout(row1, BoxLayout.LINE_AXIS));
+        row2.setLayout(new BoxLayout(row2, BoxLayout.LINE_AXIS));
+        row3.setLayout(new BoxLayout(row3, BoxLayout.LINE_AXIS));
+        row4.setLayout(new BoxLayout(row4, BoxLayout.LINE_AXIS));
+        row5.setLayout(new BoxLayout(row5, BoxLayout.LINE_AXIS));
+
+        // Add components to each of the row
+        row1.add(Box.createHorizontalGlue());
+        row1.add(btnClear);
+        row1.add(btnDelete);
+        row2.add(numBtn[7]);
+        row2.add(numBtn[8]);
+        row2.add(numBtn[9]);
+        row2.add(btnMultiply);
+        row3.add(numBtn[4]);
+        row3.add(numBtn[5]);
+        row3.add(numBtn[6]);
+        row3.add(btnAdd);
+        row4.add(numBtn[1]);
+        row4.add(numBtn[2]);
+        row4.add(numBtn[3]);
+        row4.add(btnSubtract);
+        row5.add(btnDot);
+        row5.add(numBtn[0]);
+        row5.add(btnEquals);
+        row5.add(btnDivide);
+
+        // Add all rows to the main panel
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        mainPanel.add(output);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        mainPanel.add(row1);
+        mainPanel.add(row2);
+        mainPanel.add(row3);
+        mainPanel.add(row4);
+        mainPanel.add(row5);
+
+        // Call pack() to size the window based on its contents
+        this.pack();
+
+        // Center the window on the screen
+        // this.setLocationRelativeTo(null);
+        int width = 205;
+        int height = 280;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+        int xPos = (screenWidth - width) / 2;
+        int yPos = (screenHeight - height) / 2;
+        this.setLocation(xPos, yPos);
+
+        this.add(mainPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(width, height);
+
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
